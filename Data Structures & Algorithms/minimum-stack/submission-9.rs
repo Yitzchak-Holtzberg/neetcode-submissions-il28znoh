5ca1d728e32441b603bacf6392e7cd2f8e077010
Yitzchak Holtzberg <58465047+Yitzchak-Holtzberg@users.noreply.github.com>
2026-05-29 18:13:00 -0400

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.rs
@@ -0,0 +1,26 @@
+struct MinStack {
+    stack: Vec<(i32, i32)>,
+}
+
+impl MinStack {
+    fn new() -> Self {
+        Self { stack: Vec::new() }
+    }
+
+    fn push(&mut self, val: i32) {
+        let min = self.stack.last().map_or(val, |&(_, m)| m.min(val));
+        self.stack.push((val, min));
+    }
+
+    fn pop(&mut self) {
+        self.stack.pop();
+    }
+
+    fn top(&self) -> i32 {
+        self.stack.last().unwrap().0
+    }
+
+    fn get_min(&self) -> i32 {
+        self.stack.last().unwrap().1
+    }
+}
